34bae482c7ec6babd101f526994d10ad24217f16
asisk <anniesisk@gmail.com>
2023-11-28 12:46:25 -0700

    implemented stream_counter with updated html to support turbo_stream

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -23,8 +23,13 @@ class CountersController < ApplicationController
   end
 
   def stream_counter
-    redirect_to root_path
-    # render turbo_stream: turbo_stream.update('count', html: counter.value)
+    # redirect_to root_path
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update('counter_output', html: counter.value)
+      end
+      format.html { redirect_to root_path }
+    end  
   end
 
   def tenMax

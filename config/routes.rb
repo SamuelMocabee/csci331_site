6ec9e5aff04a41d7f9b7c8c4e62ff6e7db805f43
Sam <sam.mocabee@gmail.com>
2023-11-28 16:00:32 -0700

    to-do list implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :lists
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
   post 'incrementBy1', to: "counters#incrementBy1"
   post 'incrementBy2', to: "counters#incrementBy2"
 
+  get "lists", to: "lists#index"
 end
